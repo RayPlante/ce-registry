947c24ad6a79a56e0f941ca36daad710a79710c4
Ray Plante <raymond.plante@nist.gov>
2022-03-11 06:40:20 -0500

    add in doi resolving; use smarter draft structure

--- cerr_curate_app/cerr_curate_app/views/user/draft.py
+++ cerr_curate_app/cerr_curate_app/views/user/draft.py
@@ -2,7 +2,8 @@
 The module that provides the user views for creating and editing a draft record.  
 """
 from collections import OrderedDict
-import json, pdb
+from collections.abc import Mapping
+import json, re, pdb
 
 from django.shortcuts import render
 from django.http import HttpResponse, Http404, HttpResponse, HttpResponseRedirect
@@ -23,6 +24,7 @@ def start(request):
         form = StartForm(request.POST, request.FILES)
         if form.is_valid():
             try:
+                pdb.set_trace()
                 draft = start_to_draftdoc(form.cleaned_data, request.FILES.get('xmlfile'))
                 name = draft['name']
                 del draft['name']
@@ -73,15 +75,19 @@ def start_to_draftdoc(startdata, file_submission=None):
     """
 
     if file_submission is None or startdata['start_meth'] == 'create':
-        draft = OrderedDict([
-            ("identity", OrderedDict()),
-            ("content",  OrderedDict())
-        ])
+        draft = Node(_schema)
         if startdata['create'].get('homepage'):
-            draft['content']['landingPage'] = startdata['create'].get('homepage')
-        draft['@status'] = 'active'
-        draft = OrderedDict([('Resource', draft), ('name', startdata['create'].get('name'))])
-        return draft
+            draft.add('Resource/content/landingPage', startdata['create'].get('homepage'))
+        draft.add('Resource/@status', 'active')
+        draft.add('name', startdata['create'].get('name'))
+
+        if startdata['create'].get('scrape'):
+            hp = startdata['create'].get('homepage');
+            if hp and (hp.startswith('doi:') or hp.startswith('https://doi.org/')):
+                doi_into_draftdoc(hp, draft)
+        
+        out = draft.todict()
+        return {'Resource': out['Resource'][0], 'name': out['name'][0]}
 
     elif file_submission:
         draft = load_uploaded_file(file_submission)
@@ -107,12 +113,11 @@ def draftdoc_to_edit(draft_doc, draft_id):
     return data
 
 def edit_to_draftdoc(data):
-    draft = OrderedDict([('identity', OrderedDict()),
-                         ('content',  OrderedDict())])
+    draft = Node(_schema)
     if data.get('homepage'):
-        draft['content']['landingPage'] = data.get('homepage','')
-    draft = OrderedDict([('Resource', draft)])
-    return draft
+        draft.add('Resource/content/landingPage', data.get('homepage',''))
+    out = draft.todict()
+    return {'Resource': out['Resource'][0]}
 
 def save_new_draft(draftdoc, name, request):
     """
@@ -165,3 +170,124 @@ class Http501(DetectedFailure):
 def handleFailure(exc):
     if isinstance(exc, DetectedFailure):
         return HttpResponse(status=exc.status_code, reason=exc.reason_phrase)
+
+_schema = [
+    ("Resource", [
+        ("identity", ["title", "altTitle", "identifier", "logo"]),
+        ("providers", [ "publisher", "publicationYear", "creator", "constributor", "date", "contact" ]),
+        ("content", [ "description", "subject", "landingPage", "reference", "primaryAudience" ]),
+        ("role", []),
+        ("@atts", [ 'localid', 'status'])
+    ]),
+    ("Organization", ['type']),
+    ("LiteratureResource", ['type']),
+    ("ServiceAPI", ['type', 'baseURL', 'documentationURL', 'specificationURL', 'complianceID']),
+    ("Software", ['type']),
+    ("Tool", ['type', 'codeLanguae','supportedSystem','licenseName', 'feature','documentation',
+              'validationInfo','exportControlStatement','inputOutputs','useNotes']),
+    ("DataCollection", ['type']),
+    ("Dataset", ['type', 'dataOrigin']),
+    ("Database", ['type']),
+    ("WebSite", ['type'])
+]
+
+class Node(Mapping):
+    def __init__(self, elems=[]):
+        self._data = OrderedDict()
+        self._cmplx = {}
+        for el in elems:
+            if isinstance(el, (tuple,list)):
+                self._cmplx[el[0]] = list(el[1])
+                el = el[0]
+            self._data[el] = None
+
+    def isempty(self):
+        return all([e is None for e in self._data.values()])
+
+    def __getitem__(self, key):
+        out = self._data[key]
+        if out is None:
+            raise KeyError(key)
+        return out
+
+    def __contains__(self, key):
+        v = self.get(key)
+        return v is not None
+
+    def __iter__(self):
+        return self.todict().items().__iter__()
+
+    def __len__(self):
+        return len([v for v in self._data.values() if v is not None])
+
+    _idxre = re.compile(r'\[(\-?\d+)\]$')
+    def add(self, key, val=None):
+        steps = key.split('/')
+        node = self
+        while len(steps) > 1:
+            first = steps.pop(0)
+            idx=-1
+            m = self._idxre.search(first)
+            if m:
+                idx = int(m.group(1))
+                first = self._idxre.sub('', first)
+            node = node._add(first, idx=idx)
+
+        return node._add(steps[0], val)
+        
+
+    def _add(self, key, val=None, idx=-1):
+        vl = self._data.get(key)
+        if vl is None:
+            vl = []
+            self._data[key] = vl
+
+        if val is None:
+            if len(vl) > 0:
+                return vl[idx]
+
+            # am reaching down into hierarchy
+            if key in self._cmplx:
+                val = Node(self._cmplx[key])
+            else:
+                val = Node()
+        vl.append(val)
+        return val
+
+    def todict(self):
+        data = []
+        for k,v in self._data.items():
+            if not isinstance(v, list):
+                if v is not None:
+                    data.append((k,v))
+            else:
+                children = []
+                data.append((k, children))
+                for i,e in enumerate(v):
+                    if isinstance(e, Node):
+                        e = e.todict()
+                    children.append(e)
+        return OrderedDict(data)
+
+                                
+        
+        
+
+import logging
+import nistoar.doi 
+nistoar.doi.set_client_info("NIST Circular Economy Registry",
+                            "beta", "https://nist.gov", "raymond.plante@nist.gov")
+doilog = logging.getLogger("doi")
+
+def doi_into_draftdoc(doi, draft):
+    try:
+        md = nistoar.doi.resolve(doi, logger=doilog)
+        draft.add("Resource/identity/title", md.data['title'])
+        draft.add("Resource/content/reference/@pid", doi)
+        draft.add("Resource/content/reference/#text", md.citation_text)
+    except Exception as ex:
+        pass
+    
+    return draft
+
+
